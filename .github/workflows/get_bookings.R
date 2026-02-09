4ed109a09205b9d2e8823d6a44d80b03e74219f3
Christian Knudsen <cbk@kb.dk>
2026-02-09 10:03:35 +0100

    Noter til videre arbejde

---
+++ .github/workflows/get_bookings.R
@@ -0,0 +1,31 @@
+# Henter bookings fra en specifik - hvad det nu hedder, library lighthouse. Det niveau
+
+# Det er 3087 vi interesserer os for her.
+# Vi skal tænke over hvilket tidsinterval vi skal have fat på.
+# Det særligt relevante vi får retur her er booking-id'er
+# Vi får også oplysninger tilbage om hvem der bookede. Men vi supplerer med get_booking_details.
+# Vi får også oplysninger om evt. aflysning. Herunder hvornår bookingen blev aflyst.
+
+library(lubridate)
+library(httr)
+library(dplyr)
+  dato <- today() %m-% months(2)
+  dato <- as.character(dato)
+dato
+
+  url <- modify_url(
+    url = "https://kubkalender.kb.dk",
+    path = c("1.1", "space" ,"bookings"),
+    query = list(
+      lid = 3087,
+      date = dato,
+      days = 365,
+      limit = 500
+    )
+  )
+test_data <-   GET(url, add_headers('Authorization' = paste("bearer", token))) |> 
+    content()
+
+length(test_data)
+
+test_data |> bind_rows() |> View()
